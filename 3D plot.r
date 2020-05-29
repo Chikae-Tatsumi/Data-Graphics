1c821892a735d77dc6cdc859c9d4435a8c62ac78
Chikae <65932847+Chikae-Tatsumi@users.noreply.github.com>
2020-05-29 11:08:10 +0900

    Add files via upload

---
+++ 3D plot.r
@@ -0,0 +1,16 @@
+library(rgl)
+
+setwd("~/R/Analysis/1_Test")
+METADATA<-read.csv("metadata.csv")
+
+x<-METADATA[,1] #CHANGE ME
+y<-METADATA[,2] #CHANGE ME
+z<-METADATA[,3] #CHANGE ME
+
+plot3d(x, y, z, xlab="a", ylab="b", zlab="c")
+fit <- lm(z ~ x + y)
+coefs <- coef(fit)
+planes3d(coefs[2], coefs[3], -1, coefs[1], col="blue", alpha=0.5) #Draw the planar
+
+
+snapshot3d("3D.png")
