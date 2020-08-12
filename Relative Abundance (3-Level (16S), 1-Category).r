b3ec128c6a8707c883ef35e6db50f204e240ae16
Chikae <65932847+Chikae-Tatsumi@users.noreply.github.com>
2020-08-12 10:16:51 +0900

    Update Relative Abundance (3-Level (16S), 1-Category).r

--- Relative Abundance (3-Level (16S), 1-Category).r
+++ Relative Abundance (3-Level (16S), 1-Category).r
@@ -105,7 +105,7 @@ bind <- cbind(Group, Abundance, Example)
 data<-rbind (data,bind)}
 data$Abundance <- as.numeric(as.character(data$Abundance))
 
-rownames<-rownames(phylum.class.table)[nrow(phylum.class.table):1]
+rownames<-colnames(table)[ncol(table):]
 data$Example <- factor(data$Example, levels = rownames)
 
 # ggplot
