2b1fbec0084147e0ff9751c476389b88a1e12604
Chikae <65932847+Chikae-Tatsumi@users.noreply.github.com>
2021-01-06 23:02:35 +0900

    Add files via upload

---
+++ Network_analysis_with_subset.r
@@ -0,0 +1,56 @@
+library(igraph)  
+library(Hmisc)  
+library(Matrix)  
+
+# Import files
+setwd("~/R/Analysis/1_Test")
+DESIGN <- read.csv("experimental_design.csv")
+setwd("~/R/Analysis/1_Test/ITS")
+ASV.table <- read.table(file="rarefied_ASV_table.txt",header=T)
+ASV <- ASV.table [,1:(ncol(ASV.table)-7)]
+taxonomy <- ASV.table [,(ncol(ASV.table)-6):ncol(ASV.table)]
+percent <- ASV / mean(colSums(ASV)) *100
+percent.t <- t(percent)
+
+percent.t.filter <- percent.t[ ,colSums(percent.t) >= 0.1]
+print(c(ncol(percent.t),"versus",ncol(percent.t.filter)))
+
+bind <- cbind (percent.t.filter,DESIGN)
+subset <- subset(bind, bind$Factor1=="0") # Change
+subset <- subset(subset, bind$UorR=="Urban") # Change
+
+subset.t.filter <- subset[,1:(ncol(subset)-ncol(DESIGN))]
+
+percent.cor <- rcorr(as.matrix(subset.t.filter), type="spearman")
+percent.pval <- forceSymmetric(percent.cor$P) # Self-correlation as NA
+#Select only the taxa for the filtered ASVs by using rownames of percent.pval
+sel.tax <- taxonomy[rownames(percent.pval),,drop=FALSE]
+#Sanity check --> should be "[1] TRUE"
+all.equal(rownames(sel.tax), rownames(percent.pval))
+
+p.yes <- percent.cor$P<0.05
+r.yes <- percent.cor$r>0
+r.high <- percent.cor$r>0.7
+r.val = percent.cor$r # select all the correlation values 
+p.r.yes = p.yes*r.yes*r.val*r.high
+adjm<-p.r.yes 
+
+net.grph=graph.adjacency(adjm,mode="undirected",weighted=TRUE,diag=FALSE)
+
+# hs <- hub_score(net.grph, weights=NA)$vector　
+# as <- authority_score(net.grph, weights=NA)$vector
+# pr <- page.rank(net.grph,directed=F)$vector　
+deg <- degree(net.grph, mode="all")　
+
+col=rainbow(length(levels(taxonomy$Phylum)))
+plot.igraph(net.grph, vertex.size=deg*0.15,vertex.label=NA, vertex.color=col[unclass(taxonomy$Phylum)],layout=layout.kamada.kawai)
+# plot(net.grph, vertex.size=deg*0.15,vertex.label=NA,vertex.color=col[unclass(taxonomy$Phylum)],layout=layout.random)
+# plot(net.grph, vertex.size=deg*0.15,vertex.label=NA,vertex.color=col[unclass(taxonomy$Phylum)],layout=layout.fruchterman.reingold)
+legend(x = "bottomleft", legend = levels(taxonomy$Phylum), pch = 19, col = col, bty = "n", pt.cex=2, title = "Color legend")
+
+gsize <- gsize(net.grph)
+edge_density <- round(edge_density(net.grph),digit=5)
+text(x=1,y=-1,paste("The number of edge = ", gsize))
+text(x=1,y=-1.1,paste("edge density = ", edge_density))
+
+# Save 
