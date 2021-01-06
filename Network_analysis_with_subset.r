eed7cac98f706c79632b739a7e45cb2ce039f2a2
Chikae <65932847+Chikae-Tatsumi@users.noreply.github.com>
2021-01-07 01:49:09 +0900

    Update Network_analysis_with_subset.r

--- Network_analysis_with_subset.r
+++ Network_analysis_with_subset.r
@@ -12,14 +12,14 @@ taxonomy <- ASV.table [,(ncol(ASV.table)-6):ncol(ASV.table)]
 percent <- ASV / mean(colSums(ASV)) *100
 percent.t <- t(percent)
 
-percent.t.filter <- percent.t[ ,colMeans(percent.t) >= 0.1]
-print(c(ncol(percent.t),"versus",ncol(percent.t.filter)))
-
-bind <- cbind (percent.t.filter,DESIGN)
+bind <- cbind (percent.t,DESIGN)
 subset <- subset(bind, bind$Factor1=="A") # Change
 subset <- subset(subset, subset$Factor2=="a") # Change
 
-subset.t.filter <- subset[,1:(ncol(subset)-ncol(DESIGN))]
+subset <- subset[,1:(ncol(subset)-ncol(DESIGN))]
+
+subset.t.filter <- subset[ ,colMeans(subset) >= 0.1]
+print(c(ncol(subset),"versus",ncol(subset.t.filter)))
 
 percent.cor <- rcorr(as.matrix(subset.t.filter), type="spearman")
 percent.pval <- forceSymmetric(percent.cor$P) # Self-correlation as NA
@@ -31,7 +31,7 @@ all.equal(rownames(sel.tax), rownames(percent.pval))
 p.yes <- percent.cor$P<0.05
 r.yes <- percent.cor$r>0
 r.high <- percent.cor$r>0.7
-r.val = percent.cor$r # select all the correlation values 
+r.val <- percent.cor$r # select all the correlation values 
 p.r.yes = p.yes*r.yes*r.val*r.high
 adjm<-p.r.yes 
 
@@ -43,7 +43,8 @@ net.grph=graph.adjacency(adjm,mode="undirected",weighted=TRUE,diag=FALSE)
 deg <- degree(net.grph, mode="all")　
 
 col=rainbow(length(levels(sel.tax$Phylum)))
-plot.igraph(net.grph, vertex.size=deg*0.15,vertex.label=NA, vertex.color=col[unclass(sel.tax$Phylum)],layout=layout.kamada.kawai)
+# plot.igraph(net.grph, vertex.size=deg*0.15,vertex.label=NA, vertex.color=col[unclass(sel.tax$Phylum)],layout=layout.kamada.kawai)
+plot.igraph(net.grph, vertex.size=0.15,vertex.label=NA, vertex.color=col[unclass(sel.tax$Phylum)],layout=layout.kamada.kawai)
 # plot(net.grph, vertex.size=deg*0.15,vertex.label=NA,vertex.color=col[unclass(sel.tax$Phylum)],layout=layout.random)
 # plot(net.grph, vertex.size=deg*0.15,vertex.label=NA,vertex.color=col[unclass(sel.tax$Phylum)],layout=layout.fruchterman.reingold)
 legend(x = "bottomleft", legend = levels(sel.tax$Phylum), pch = 19, col = col, bty = "n", pt.cex=2, title = "Color legend")
