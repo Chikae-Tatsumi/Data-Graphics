f0c404e0bba81bccf92c5dc97cc1a95619579bf9
Chikae <65932847+Chikae-Tatsumi@users.noreply.github.com>
2021-01-09 21:45:10 +0900

    Update Network_analysis.r

--- Network_analysis.r
+++ Network_analysis.r
@@ -10,9 +10,24 @@ taxonomy <- ASV.table [,(ncol(ASV.table)-6):ncol(ASV.table)]
 percent <- ASV / mean(colSums(ASV)) *100
 percent.t <- t(percent)
 
+# To make minor phylum "Others"
+phylum <- aggregate(percent, by=list(taxonomy$Phylum),FUN = sum,na.rm=F) 
+row.names(phylum)<-phylum[,1]
+phylum <- phylum[,-1]
+phylum <- data.frame(phylum)
+rowMeans <- rowMeans(phylum) 
+phylum <- cbind(phylum,rowMeans)
+minor.phylum <- phylum[phylum[,"rowMeans"] < 1,] # Change
+minor.phylum.list <- rownames(minor.phylum)
+
+for (i in 1:length (minor.phylum.list)){
+taxonomy$Phylum <- gsub(minor.phylum.list[i],"Others",taxonomy$Phylum)}
+
+# Filter to pick up parts of ASVs
 percent.t.filter <- percent.t[ ,colMeans(percent.t) >= 0.01]
 print(c(ncol(percent.t),"versus",ncol(percent.t.filter)))
 
+# Calculate network
 percent.cor <- rcorr(as.matrix(percent.t.filter), type="spearman")
 percent.pval <- forceSymmetric(percent.cor$P) # Self-correlation as NA
 #Select only the taxa for the filtered ASVs by using rownames of percent.pval
@@ -34,6 +49,18 @@ net.grph=graph.adjacency(adjm,mode="undirected",weighted=TRUE,diag=FALSE)
 # pr <- page.rank(net.grph,directed=F)$vector　
 deg <- degree(net.grph, mode="all")　
 
+# Align taxonomy names
+sel.tax$Phylum <- factor(sel.tax$Phylum)
+others.n <- which(levels(sel.tax$Phylum)=="Others")
+levels <- NULL
+for (i in 1:(others.n-1)){
+    levels <- c(levels, levels(sel.tax$Phylum)[i])}
+for (i in (others.n+1):(length(levels(sel.tax$Phylum)))){
+    levels <- c(levels, levels(sel.tax$Phylum)[i])}
+levels <- c(levels, "Others")
+levels(sel.tax$Phylum) <- levels
+
+# Illustrate
 col=rainbow(length(levels(sel.tax$Phylum)))
 plot.igraph(net.grph, vertex.size=deg*0.15,vertex.label=NA, vertex.color=col[unclass(sel.tax$Phylum)],layout=layout.kamada.kawai)
 # plot(net.grph, vertex.size=deg*0.15,vertex.label=NA,vertex.color=col[unclass(sel.tax$Phylum)],layout=layout.random)
@@ -45,4 +72,6 @@ edge_density <- round(edge_density(net.grph),digit=5)
 text(x=1,y=-1,paste("The number of edge = ", gsize))
 text(x=1,y=-1.1,paste("edge density = ", edge_density))
 
-# Save as you like
+# Save 
+dev.copy(pdf, file="~/R/Analysis/1_Test/ITS/Network.pdf", height=5, width=10)
+dev.off()
